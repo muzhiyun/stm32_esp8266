e1dddc818c9ac1612fe17f01c40d26bddbad37fe
muzhiyun <my616422@126.com>
2019-08-25 19:42:54 +0800

    modify

--- Http_Api/post.php
+++ Http_Api/post.php
@@ -6,20 +6,64 @@ if(isset($_FILES["file"])&&(($_FILES["file"]["type"] == "image/gif")|| ($_FILES[
 }
 else
 {
-	echo try_json_encode("403","Uploaded file is illegal");
+	echo try_json_encode("403","Uploaded file is invalid");
 	exit(0);
 }
 
 
+$pic = $file["name"];
+$pics = substr($pic,0,strrpos($pic,"."));
+if(preg_match("/^\d*$/",$pics)){
+	$data = number_format($pics);
+	$time = date("Y-m-d H:i:s",$pics);
+	$path = "image/".$file["name"];
+
+}
+else
+{
+	echo try_json_encode("403","The filename is invalid");
+	exit(0);
+}
+
+
+if (isset($_POST["x"])&&isset($_POST["y"])&&isset($_POST["h"]))
+{
+	$x = $_POST["x"];
+	$y = $_POST["y"];
+	$h = $_POST["h"];
+}
+else
+{
+	echo try_json_encode("403","The parameter is invalid");
+	exit(0);
+}
+
+
+/*echo "</br>文件:".$pic;
+echo "</br>x:".$x;
+echo "</br>y:".$y;
+echo "</br>h:".$h;
+echo "</br>path:".$path;
+echo "</br>time:".$time;*/
 
 $file_name = $file["name"];
 $tmp_file = $file["tmp_name"];
-echo $tmp_file;
+
 $error = $file['error'];
     if($error == 0){
-        move_uploaded_file($tmp_file,$file_name);
+        move_uploaded_file($tmp_file,"./image/".$file_name);
+		require "connet.php";					//数据库部分
+		$sql="INSERT INTO oil (x,y,h,path,time) VALUES ($x,$y,$h,'$path','$time')";
+		$conn->query($sql);
+		$conn->close();
+		//echo "</br>sql:".$sql;
 		echo try_json_encode("0","success");
     }
+	else
+	{
+		echo try_json_encode("403","Uploaded file is illegal");
+		exit(0);
+	}
 	
 
 function try_json_encode($retcode_temp,$message_temp){
